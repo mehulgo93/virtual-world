2791f9c847c23fcae8eaa5b1642ac3530f8b9df0
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-14 14:38:47 -0500

    adding and removing points

--- js/graphEditor.js
+++ js/graphEditor.js
@@ -5,17 +5,50 @@ class GraphEditor {
 
         this.ctx = this.canvas.getContext("2d");
 
+        this.selected = null;
+        this.hovered = null;
+
+
         this.#addEventListeners();
     }
 
     #addEventListeners() {
         this.canvas.addEventListener("mousedown", (evt) => {
+            if (evt.button == 2) { // right click
+                if (this.hovered) {
+                    this.#removePoint(this.hovered);
+                }
+            }
+            if (evt.button == 0) { // left click
+                const mouse = new Point(evt.offsetX, evt.offsetY);
+                if (this.hovered) {
+                    this.selected = this.hovered;
+                    return;
+                }
+                this.graph.addPoint(mouse);
+                this.selected = mouse;
+            }
+        });
+        this.canvas.addEventListener("mousemove", (evt) => {
             const mouse = new Point(evt.offsetX, evt.offsetY);
-            this.graph.addPoint(mouse);
-        })
+            this.hovered = getNearestPoint(mouse, this.graph.points, 10);
+        });
+        this.canvas.addEventListener("contextmenu", (evt) => evt.preventDefault());
+    }
+
+    #removePoint(point) {
+        this.graph.removePoint(point);
+        this.hovered = null;
+        this.selected = null;
     }
 
     display() {
         this.graph.draw(this.ctx);
+        if (this.hovered) {
+            this.hovered.draw(this.ctx, { fill: true });
+        }
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true });
+        }
     }
 }
