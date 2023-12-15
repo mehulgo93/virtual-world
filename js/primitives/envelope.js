19113d81278307fbf7525135b1bd39c723927fcc
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2023-12-15 07:47:17 -0500

    finishing world

--- js/primitives/envelope.js
+++ js/primitives/envelope.js
@@ -1,28 +1,29 @@
 class Envelope {
-    constructor(skeleton, width) {
+    constructor(skeleton, width, roundness = 0) {
         this.skeleton = skeleton;
-        this.poly = this.#generatePolygon(width);
+        this.poly = this.#generatePolygon(width, roundness);
     }
 
-    #generatePolygon(width) {
+    #generatePolygon(width, roundness) {
         const { p1, p2 } = this.skeleton;
         const radius = width / 2;
         const alpha = angle(subtract(p1, p2));
         const alpha_cw = alpha + Math.PI / 2;
         const alpha_ccw = alpha - Math.PI / 2;
         const points = [];
-        const step = Math.PI / 10;
-        for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+        const step = Math.PI / Math.max(1, roundness);
+        const eps = step / 2;
+        for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
             points.push(translate(p1, i, radius));
         }
-        for (let i = alpha_ccw; i <= alpha_cw; i += step) {
+        for (let i = alpha_ccw; i <= alpha_cw + eps; i += step) {
             points.push(translate(p2, Math.PI + i, radius));
         }
 
         return new Polygon(points);
     }
 
-    draw(ctx) {
-        this.poly.draw(ctx);
+    draw(ctx, options) {
+        this.poly.draw(ctx, options);
     }
 }
---
+++ js/world.js
@@ -0,0 +1,34 @@
+class World {
+    constructor(graph, roadWidth = 100, roadRoundness = 10) {
+        this.graph = graph;
+        this.roadWidth = roadWidth;
+        this.roadRoundness = roadRoundness;
+
+        this.envelopes = [];
+        this.roadBorders = [];
+
+        this.generate();
+    }
+
+    generate() {
+        this.envelopes.length = 0;
+        for (const seg of this.graph.segments) {
+            this.envelopes.push(
+                new Envelope(seg, this.roadWidth, this.roadRoundness)
+            );
+        }
+        this.raodBorders = Polygon.union(this.envelopes.map((e) => e.poly));
+    }
+
+    draw(ctx) {
+        for (const env of this.envelopes) {
+            env.draw(ctx, { fill: "#BBB", stroke: "#BBB", lineWidth: 15 });
+        }
+        for (const seg of this.graph.segments) {
+            seg.draw(ctx, { color: "white", width: 4, dash: [10, 10] });
+        }
+        for (const seg of this.raodBorders) {
+            seg.draw(ctx, { color: "white", width: 4 });
+        }
+    }
+}
